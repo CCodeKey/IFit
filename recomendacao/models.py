e893fa35702497dd146bed89abfb401a66334ef6
CodeKey <newcodiguin@gmail.com>
2025-01-15 11:26:21 -0300

    feat: criar usuario e urls(login/signin)

--- recomendacao/models.py
+++ recomendacao/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=8)
+    sobrenome = models.CharField(max_length=20)
+    idade = models.IntegerField()
+    email = models.EmailField()
+    senha = models.CharField(max_length=50)
+    
+    def __str__(self):
+        nome = self.nome 
+        sobrenome = self.sobrenome
+        return f"{nome} {sobrenome}"
+
+# Falta fazer a recomendação - completo
+# Construir o sistema de login e signin
--- recomendacao/views.py
+++ recomendacao/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Usuario
+from django.views.generic import CreateView
 
-# Create your views here.
+def home(request):
+    return render(request, "recomendacao/home.html")
+
+def login(request):
+    return render(request, "recomendacao/login.html")
+
+class UsuarioCreateView(CreateView):
+    model = Usuario
+    fields = ['nome','sobrenome','idade','email','senha']
+    success_url = "login"
